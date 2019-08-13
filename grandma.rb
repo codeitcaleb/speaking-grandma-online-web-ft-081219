afea6c1333b442899893928901b17652481ad030
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-13 02:27:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(string = 'I  YOU GRANDMA!')
+def speak_to_grandma(string = 'I LOVE YOU GRANDMA!')
   
   if string.upcase do
     return "I LOVE YOU TOO PUMPKIN!"
