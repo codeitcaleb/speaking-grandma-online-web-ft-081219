45927c7727a88ccb5303d206ab314d8337f8c9c4
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-13 02:49:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,17 @@
 def speak_to_grandma(string = 'I LOVE YOU GRANDMA!')
   puts string
   
+  if string == 'Hi!'
+    puts 'HUH?! SPEAK UP, SONNY'
+  
   if string == 'Hi Nana, how are you?'
-    puts
+    puts 'HUH?! SPEAK UP, SONNY'
+ 
+ if string == 'WHAT DID YOU EAT TODAY?'
+   puts 'NO, NOT SINCE 1938!'
   
   if string == string.upcase
-  else print 
+   print 'I LOVE YOU TOO PUMPKIN!'
   # return "I LOVE YOU TOO PUMPKIN!"
 end  
 
