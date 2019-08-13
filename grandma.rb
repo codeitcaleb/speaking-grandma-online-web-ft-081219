7f7df743ba6eee1f5459e50c5e7d2f217a9d2516
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-13 03:07:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -27,6 +27,10 @@ def speak_to_grandma(string = 'I LOVE YOU GRANDMA!')
    return 'NO, NOT SINCE 1938!'
  end
  
+ if string == 'WHAT?'
+   return 'NO, NOT SINCE 1938!'
+ end
+ 
   if string == string.upcase
     return "I LOVE YOU TOO PUMPKIN!"
   end  
